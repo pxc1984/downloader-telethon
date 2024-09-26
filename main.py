62cdfca1c2f74e22135a6621cc763bbecab1dac9
pxc1984 <igor.mamaev1@gmail.com>
2024-09-26 13:47:06 +0300

    linter fix

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
-#!/usr/bin/env python3
+"""Main module for this project"""
 
-import logging
 import asyncio
 import toml
-import datetime
-import random
 from telethon import TelegramClient
-from telethon.errors import RPCError, PeerFloodError, ChatAdminRequiredError, UserNotParticipantError
 
 config = toml.load('config.toml')
 
 
 async def run_bot():
+    """Function, responsible for main loop"""
+    client = TelegramClient( 
+        config['app']['client_name'], 
+        config['telegram']['api_id'], 
+        config['telegram']['api_hash']
+    )
+
     await client.start(config['telegram']['phone_number'])
     while True:
         await asyncio.sleep(60)
 
 
 if __name__ == '__main__':
-    client = TelegramClient(
-        config['app']['client_name'], 
-        config['telegram']['api_id'], 
-        config['telegram']['api_hash']
-    )
-    
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run_bot())
